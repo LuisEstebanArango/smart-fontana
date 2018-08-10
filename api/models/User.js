9951f6ac5f53b0aa6b24036911c1aad0ba3613a2
luisesteban <estebanarango14@hotmail.com>
2018-08-10 11:36:52 -0500

    Se crea model y controller de user

---
+++ api/models/User.js
@@ -0,0 +1,56 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    name : {
+      type: 'string',
+      required: true
+    }, 
+    last_name : {
+      type: 'string',
+      required: true
+    },
+    identification : {
+      type: 'string',
+      required: true
+    },
+    email : {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+    tipo : {
+      type: 'string',
+      isIn: ["TEACHER", "STUDENT", "ADMIN"], // TODO cambiar a constantes
+      required: true
+    },
+    grado : {
+      type: 'string',
+      isIn: ["k2", "k3", "k4", "T", "1A", "1B", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"], // TODO cambiar a constantes
+      allowNull: true
+    }
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
